d6f70faffc296129f296ea74417ff77f8fbbc8e8
EvrenTank <evrentank@gmail.com>
2023-11-16 12:29:50 +0300

    add files first time

---
+++ src/app/navbar/navbar.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+import styles from '../../../styles/navbar/Navbar.module.scss';
+const Navbar =() =>{
+    return(
+        <div className={styles.navbar}>
+            <Image src='/navbarIcon.png' alt='icon' width={300} height={120} ></Image>
+        </div>
+    )
+}
+
+export default Navbar;
